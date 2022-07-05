0169f97783dc091c2c4189507f43b937918b9b27
favouronyi <amandafavour123@gmail.com>
2022-07-05 15:21:40 -0700

    main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,14 @@
-int _putchar(char c);
-void print_alphabet(void)
+nclude "main.h"
+
+/**
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		print_alphabet();
+			return (0);
+}
 
 
